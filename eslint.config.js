7e124dffdd7f3bf44593f21ce00fab42dff5ee78
Muhammad Azqi Madani Ardan <muhammadazqi098@gmail.com>
2025-05-19 17:28:04 +0700

    first-comit

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs,jsx}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        rules: {
+            'no-console': ['error'],
+            'prefer-const': ['error'],
+            camelcase: ['error'],
+        },
+    },
+    {
+        files: ['**/*.{js,mjs,cjs,jsx}'],
+        languageOptions: { globals: globals.browser },
+    },
+    pluginReact.configs.flat.recommended,
+    globalIgnores(['node_modules/', 'public/']),
+])
